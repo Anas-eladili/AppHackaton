8e974604661ef8f83336d921feca32004216e4e8
anasel <anaseladili@outlook.fr>
2021-10-06 23:42:52 +0200

    hello

---
+++ src/main/java/com/hackathon/hackathonepiseneepcrous/HackathonEpisenEepCrousApplication.java
@@ -0,0 +1,12 @@
+package com.hackathon.hackathonepiseneepcrous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonEpisenEepCrousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonEpisenEepCrousApplication.class, args);
+	}
+}
